73e9b8e9609c262ea768c670982e510eee57299b
madis <madis.pihlasalu@gmail.com>
2022-02-16 18:28:27 +0200

    updates

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,12 +1,36 @@
-import { createConnection, ConnectionOptions } from 'typeorm';
+import { createConnection } from 'typeorm';
+import path from 'path';
+import config from '../config/config.json';
+import bunyan, { stdSerializers } from 'bunyan';
+import { Stream } from 'stream';
+import { serialize } from 'v8';
+
+const stream = new Stream(); 
+
+const logger = bunyan.createLogger({
+  name: "database",
+  serializers: bunyan.stdSerializers,
+  stream: process.stdout
+});
 
 export async function openDatabaseConnection() {
   // await closeDatabaseConnection();
 
-  const conn = await createConnection();
+  const conn = await createConnection({
+    type: "mysql",
+    entities: [path.resolve(__dirname, '..', 'entities/*{.ts,.js}')],
+    host: config.host,
+    port: config.port,
+    database: "blog",
+    username: config.username,
+    password: config.password,
+    synchronize: true
+  });
 
   if (!conn.isConnected) {
     throw new Error('Connection to database failed');
   }
+
+  logger.info('Database connected: ', conn.isConnected);
   return conn;
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,12 +3,12 @@ import fs from 'fs';
 import createUser from './users/createUser';
 import getUser from './users/getUser';
 import getUsers from './users/getUsers';
+import deleteUser from './users/deleteUser';
 import getPost from './posts/getPost';
 import getPosts from './posts/getPosts';
 import createPost from './posts/createPost';
-
 const router = express.Router();
-var userRoutes: string[] = [];
+// var userRoutes: string[] = [];
 
 // fs.readdirSync(__dirname + '/user').forEach(function (file) {
 //   router.use('/users', async () => {
@@ -17,7 +17,7 @@ var userRoutes: string[] = [];
 // });
 // console.log(userRoutes.toString());
 
-router.use('/users', [createUser, getUser, getUsers]);
-router.use('/posts',[createPost, getPost, getPosts]);
+router.use('/users', [createUser, getUser, getUsers, deleteUser]);
+router.use('/posts', [createPost, getPost, getPosts]);
 
 export default router;
